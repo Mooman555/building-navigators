65f8d3e4aeef479a3e817a59958041edd041431d
zahra <zahra@karigar.pk>
2023-01-04 17:16:07 +0500

    data entry

--- src/Components/SearchableDropDown/DropDownValues.js
+++ src/Components/SearchableDropDown/DropDownValues.js
@@ -32,215 +32,257 @@ export const destinations = [
       {
         label: "Level 4 Storage Area             Level 4",
         value: 6,
+        id:"level4StorageArea4",
         number: '4',
       },
       {
         label: "Storage Area                           Level 4",
         value: 7,
+        id:"storageAreaLeft4",
         number: '4',
       },
       {
         label: "Guest Elevators                      Level 4",
         value: 8,
+        id:"guestElevators4",
         number: '4',
       },
       {
         label: "Mechanical Room                  Level 4",
         value: 9,
+        id:"mechanicalRoom4",
         number: '4',
       },
       {
-        label: "Public Stairs                           Level 4",
+        label: "Public Stairs Down Right                           Level 4",
         value: 10,
+        id:"publicStairsDownRight4",
         number: '4',
       },
   
       {
         label: "Room # 401                           Level 4",
         value: 11,
+        id:"room#401_4",
         number: '4',
       },
       {
         label: "Room # 402                           Level 4",
         value: 12,
+        id:"room#402_4",
         number: '4',
       },
       {
         label: "Room # 403                           Level 4",
         value: 13,
+        id:"room#403_4",
         number: '4',
       },
       {
         label: "Room # 404                           Level 4",
         value: 14,
+        id:"room#404_4",
         number: '4',
       },
       {
         label: "Room # 405                           Level 4",
         value: 15,
+        id:"room#405_4",
         number: '4',
       },
       {
         label: "Room # 406                           Level 4",
         value: 16,
+        id:"room#406_4",
         number: '4',
       },
       {
         label: "Room # 407                           Level 4",
         value: 17,
+        id:"room#407_4",
         number: '4',
       },
       {
         label: "Room # 408                           Level 4",
         value: 18,
+        id:"room#408_4",
         number: '4',
       },
       {
         label: "Room # 409                           Level 4",
         value: 19,
+        id:"room#409_4",
         number: '4',
       },
       {
         label: "Facility Storage Area            Level 4",
         value: 20,
+        id:"facilityStorageArea4",
         number: '4',
       },
   
       {
         label: "Electrical Area                       Level 4",
         value: 21,
+        id:"electricalArea4",
         number: '4',
       },
       {
         label: "Roof                                        Level 4",
         value: 22,
+        id:"roof4",
         number: '4',
       },
       {
         label: "Linen Room                           Level 4",
         value: 23,
+        id:"linenRoom4",
         number: '4',
       },
       {
         label: "Food & Beverage Office       Level 4",
         value: 24,
+        id:"food&BeverageOffice4",
         number: '4',
       },
       {
         label: "Kitchen Management Offices- Level 4",
         value: 25,
+        id:"kitchenManagementOffices4",
         number: '4',
       },
       {
         label: "Kitchen                                  Level 4",
         value: 26,
+        id:"kitchen4",
         number: '4',
       },
       {
         label: "Silver Room                          Level 4",
         value: 27,
+        id:"silverRoom4",
         number: '4',
       },
       {
         label: "Storage Area                        Level 4",
         value: 28,
+        id:"storageAreaRight4",
         number: '4',
       },
       {
         label: "Kitchenware Storage Room-    Level 4",
         value: 29,
+        id:"kitchenwareStorageRoom4",
         number: '4',
       },
       {
         label: "Rainier Vista Terrace           Level 4",
         value: 30,
+        id:"rainierVistaTerrace4",
         number: '4',
       },
       {
         label: "Kitchen Conference Access-  Level 4",
         value: 31,
+        id:"kitchenConferenceAccess4",
         number: '4',
       },
   
       {
         label: "Catwalk Storage                     Level 3",
         value: 32,
+        id:"catwalkStorage3",
         number: '3',
       },
       {
         label: "Reception                                Level 3",
         value: 33,
+        id:"reception3",
         number: '3',
       },
       {
         label: "Electrical Room                      Level 3",
         value: 34,
+        id:"electricalRoom3",
         number: '3',
       },
       {
         label: "Coat Room                              Level 3",
         value: 35,
+        id:"coatRoom3",
         number: '3',
       },
       {
         label: "Conference Room # 301      Level 3",
         value: 36,
+        id:"conferenceRoom#301_3",
         number: '3',
       },
   
       {
         label: "Conference Room # 302      Level 3",
         value: 37,
+        id:"conferenceRoom#302_3",
         number: '3',
       },
       {
         label: "Conference Room # 303      Level 3",
         value: 38,
+        id:"conferenceRoom#303_3",
         number: '3',
       },
       {
         label: "Service Store                         Level 3",
         value: 39,
+        id:"serviceStore3",
         number: '3',
       },
       {
         label: "Staff Kitchen                         Level 3",
         value: 40,
+        id:"staffKitchen3",
         number: '3',
       },
       {
         label: "CEO Office                             Level 3",
         value: 41,
+        id:"ceoOffice3",
         number: '3',
       },
       {
         label: "Staffe Cubicles Area           Level 3",
         value: 42,
+        id:"staffeCubiclesArea3",
         number: '3',
       }, {
         label: "Executive Office                   Level 3",
         value: 43,
+        id:"executiveOffice3",
         number: '3',
       },
       {
         label: "Waiting Area                         Level 3",
         value: 44,
+        id:"waitingArea3",
         number: '3',
       },
       {
         label: "IT Department                      Level 3",
         value: 45,
+        id:"itDepartment3",
         number: '3',
       },
       {
         label: "Staff Lounge                         Level 3",
         value: 46,
+        id:"staffLounge3",
         number: '3',
       },
   
       {
         label: "Storage Area                         Level 3",
         value: 47,
+        id:"storageArea3",
         number: '3',
       },
   
@@ -248,11 +290,13 @@ export const destinations = [
       {
         label: "Green Room                          Level 2",
         value: 48,
+        id:"greenRoom2",
         number: '2',
       },
       {
         label: "Backstage Theater               Level 2",
         value: 49,
+        id:"backstageTheater2",
         number: '2',
       },
       {
@@ -312,20 +356,24 @@ export const destinations = [
       {
         label: "Facility & Custodial Offices   Level 2",
         value: 59,
+        id:"facility&CustodialOffices2",
         number: '2',
       },
       {
         label: "Ops Lobby                               Level 2",
         value: 60,
+        id:"opsLobby2",
         number: '2',
       }, {
         label: "Technical Service & M&E Offices-Level 2",
         value: 61,
+        id:"technicalService&M&EOffices2",
         number: '2',
       },
       {
         label: "Boiler Room                             Level 2",
         value: 62,
+        id:"boilerRoom2",
         number: '2',
       },
       {
@@ -344,113 +392,136 @@ export const destinations = [
       {
         label: "NW Emergency-                      Level 1",
         value: 65,
+        id:"nwEmergency1",
         number: '1',
       },
   
       {
         label: "Mechanical Room                  Level 1",
         value: 66,
+        id:"mechanicalRoom1",
         number: '1',
       },
   
       {
         label: "SW Hall Lobby Elevator         Level 1",
         value: 67,
+        id:"swHallLobbyElevator1",
         number: '1',
       },
       {
         label: "Public Stairs                            Level 1",
         value: 68,
+        id:"publicStairs1",
         number: '1',
       },
       {
         label: "Theater Emergency Exit        Level 1",
         value: 69,
+        id:"theaterEmergencyExit1",
         number: '1',
       },
       { 
         label: "Orchestra Pit Enterance        Level 1",
         value: 70,
+        id:"orchestraPitEnterance1",
         number: '1',
       },
       {
         label: "Back Stage Enterance           Level 1",
         value: 71,
+        id:"backStageEnterance1",
         number: '1',
       },
       {
         label: "Theater Storage                     Level 1",
         value: 72,
+        id:"theaterStorage1",
         number: '1',
       },
       {
         label: "Center Hall Lobby                  Level 1",
         value: 73,
+        id:"centerHallLobby1",
         number: '1',
       },
       {
         label: "Theater Storage SW              Level 1",
         value: 74,
+        id:"theaterStorageSw1",
         number: '1',
       },
       {
         label: "Mechanical Room SW          Level 1",
         value: 75,
+        id:"mechanicalRoomSw1",
         number: '1',
       },
       {
         label: "Main Enterance                      Level 1",
         value: 76,
+        id:"mainEnterance1",
         number: '1',
       },
       {
         label: "Enterance Hall A                    Level 1",
         value: 77,
+        id:"enteranceHallA1",
         number: '1',
       },
       {
-        label: "Electrical Room                     Level 1",
+        label: "Electrical Room Bottom                     Level 1",
         value: 78,
+        id:"electricalRoomBottom1",
         number: '1',
       },
       {
         label: "Enterance Hall B                    Level 1",
         value: 79,
+        id:"enteranceHallB1",
         number: '1',
       },
   
       {
         label: "Loading/Unloading Doors    Level 1",
         value: 80,
+        id:"loading/UnloadingDoors1",
         number: '1',
       }, {
         // label: "North East Stairwell/Exit Stairs",
         label: "Exit Stairs                               Level 1",
         value: 81,
+        id:"exitStairs1",
         number: '1',
       }, {
         label: "Main Loading Dock               Level 1",
         value: 82,
+        id:"mainLoadingDock1",
         number: '1',
       }, {
         label: "Security/Receiving                Level 1",
         value: 83,
+        id:"security/Receiving1",
         number: '1',
       }, {
         label: "Equipment Room                  Level 1",
         value: 84,
+        id:"equipmentRoom1",
         number: '1',
       }, {
         label: "Storage                                   Level 1",
         value: 85,
+        id:"storage1",
         number: '1',
       }, {
         label: "Telephone Room                  Level 1",
         value: 86,
+        id:"telephoneRoom1",
         number: '1',
       }, {
         label: "Storage Room                       Level 1",
         value: 87,
+        id:"storageRoom1",
         number: '1',
       }, 
       {
@@ -513,6 +584,7 @@ export const destinations = [
       {
         label: "Backstage Theater             Level 2M",
         value: 89,
+        id:"backstageTheater2M",
         number: '2M',
       },
       {
@@ -554,6 +626,7 @@ export const destinations = [
       {
         label: "Theater Booth                    Level 2M",
         value: 96,
+        id:"theaterBooth2M",
         number: '2M',
       },
       {
@@ -565,7 +638,197 @@ export const destinations = [
       {
         label: "Exit                                      Level 2M",
         value: 98,
+        id:"exit2M",
         number: '2M',
+      },
+
+      {
+        label: "Rest Room Down-  Level 4",
+        value: 99,
+        id:"restRoomDown4",
+        number: '4',
+      },
+
+      {
+        label: "Rest Room Right-  Level 4",
+        value: 100,
+        id:"restRoomRight4",
+        number: '4',
+      },
+
+      {
+        label: "Show Manager Room-  Level 4",
+        value: 101,
+        id:"showManagerRoom4",
+        number: '4',
+      },
+
+      {
+        label: "Rest Room Top-  Level 4",
+        value: 102,
+        id:"restRoomTop4",
+        number: '4',
+      },
+
+      {
+        label: "Public Stairs Down Top Left                           Level 4",
+        value: 103,
+        id:"publicStairsDownTopLeft4",
+        number: '4',
+      },
+
+      {
+        label: "Elevator Top Left                      Level 4",
+        value: 104,
+        id:"elevatorTopLeft4",
+        number: '4',
+      },
+
+      {
+        label: "Elevator Bottom Right                      Level 4",
+        value: 105,
+        id:"elevatorBottomRight4",
+        number: '4',
+      },
+
+      {
+        label: "Public Stairs Up Left                           Level 4",
+        value: 106,
+        id:"publicStairsUpLeft4",
+        number: '4',
+      },
+
+      {
+        label: "Public Stairs Down Top Right                           Level 4",
+        value: 107,
+        id:"publicStairsDownTopRight4",
+        number: '4',
+      },
+
+      {
+        label: "Elevator Down Left                         Level 3",
+        value: 108,
+        id:"elevatorDownLeft3",
+        number: '3',
+      },
+
+      {
+        label: "Rest Room Left                         Level 3",
+        value: 109,
+        id:"restRoomLeft3",
+        number: '3',
+      },
+
+      {
+        label: "Rest Room Right                         Level 3",
+        value: 110,
+        id:"restRoomRight3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 6                         Level 3",
+        value: 111,
+        id:"administrationRoom6_3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 5                         Level 3",
+        value: 112,
+        id:"administrationRoom5_3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 4                         Level 3",
+        value: 113,
+        id:"administrationRoom4_3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 3                         Level 3",
+        value: 114,
+        id:"administrationRoom3_3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 2                         Level 3",
+        value: 115,
+        id:"administrationRoom2_3",
+        number: '3',
+      },
+
+      {
+        label: "Administration Room 1                         Level 3",
+        value: 116,
+        id:"administrationRoom1_3",
+        number: '3',
+      },
+
+      {
+        label: "Public Stairs Down Right                         Level 3",
+        value: 116,
+        id:"publicStairsDownRight3",
+        number: '3',
+      },
+
+      {
+        label: "Elevator                       Level 1",
+        value: 117,
+        id:"elevator1",
+        number: '1',
+      },
+      
+      {
+        label: "Electrical Stairs                       Level 1",
+        value: 118,
+        id:"electricalStairs1",
+        number: '1',
+      },
+
+      {
+        label: "Rest Room Left                       Level 1",
+        value: 119,
+        id:"restRoomLeft1",
+        number: '1',
+      },
+
+      {
+        label: "Public Stairs Down Bottom                       Level 1",
+        value: 120,
+        id:"publicStairsDownBottom1",
+        number: '1',
+      },
+
+      {
+        label: "Public Stairs Down Right                       Level 1",
+        value: 121,
+        id:"publicStairsDownRight1",
+        number: '1',
+      },
+
+      {
+        label: "Rest Room Right                       Level 1",
+        value: 122,
+        id:"restRoomRight1",
+        number: '1',
+      },
+
+      {
+        label: "Electrical Room Top                     Level 1",
+        value: 123,
+        id:"electricalRoomTop1",
+        number: '1',
+      },
+
+      {
+        label: "Electrical Room Right                     Level 1",
+        value: 124,
+        id:"electricalRoomRight1",
+        number: '1',
       },
 
       // {
